51be14871b5256464bd117b172b618900678b188
Axel <nigthmaresans2@gmail.com>
2024-02-14 19:46:11 -0600

    Repository class devel

--- src/modules/repository.py
+++ src/modules/repository.py
@@ -8,10 +8,21 @@ from modules.directories import ProjectDirs
 class Repository:
     def __init__(self):
         dirs = ProjectDirs()
-        self.config = dirs.config_dir / "repository.json"
+        self.config: pathlib.Path = dirs.config_dir / "repository.json"
         
         if self.config.exists() is False:
-            
+            content = requests.get("https://github.com/XtrrmeTHN/XtremePKG/src/repository.json").content
+
+            self.config.write_bytes(content)
         
         self.config_content = json.loads(self.config.read_text())
+
+    def generate_repo(self):
+        repo_url = "https://api.github.com/users/XtremeTHN/repos"
+
+
+    def get_pkg(self, name):
+        ...
         
+    def install_pkg(self, repo_name: str):
+        ...
